21248a8da237a1019c6d7d39efcbfc9932e1176b
NTBPhil <filipkohrasko@gmail.com>
2024-10-21 20:12:11 +0200

    Add project files.

---
+++ math/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+int width = GetValidNumber("Insert width: ");
+int height = GetValidNumber("Insert height ");
+static int GetValidNumber(string text)
+{
+    int number = 0;
+    bool isValid = false;
+
+    while (!isValid)
+    {
+        Console.WriteLine(text);
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out number))
+        {
+            isValid = true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input, please try a number.");
+        }
+    }
+    return number;
+}
+
+Console.WriteLine(width * height);
